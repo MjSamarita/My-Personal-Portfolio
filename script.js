c481fa5f281e1df65d270d2c848258e9673470ef
MJ Asuncion Samarita <mjsamarita9@gmail.com>
2025-04-14 07:23:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/* Author: Mary Joy Samarita */
+/* Portfolio Project - 2025 */
+/* Contact me for full code */
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
